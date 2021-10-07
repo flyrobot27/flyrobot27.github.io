ac4d58a1c14b49153868dddd9bb3dd26f425348e
flyrobot27 <stevenheung.ca@gmail.com>
2021-10-06 21:42:39 -0600

    Remove social widget

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,13 +23,13 @@ AUTHOR_FEED_RSS = None
 # Blogroll
 LINKS = (('Pelican', 'https://getpelican.com/'),
          ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),)
 
 # Social widget
+'''
 SOCIAL = (('You can add links in your config file', '#'),
           ('Another social link', '#'),)
-
+'''
 DEFAULT_PAGINATION = 10
 
 # Uncomment following line if you want document-relative URLs when developing
